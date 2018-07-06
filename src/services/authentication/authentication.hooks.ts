a29626aa703ab03a6a8a81c2f7dd924bc46e8aff
Ryan <hey@mix-tape.io>
2018-07-06 16:00:34 +0800

    Differentiate login error

--- src/services/authentication/authentication.hooks.ts
+++ src/services/authentication/authentication.hooks.ts
@@ -1,8 +1,27 @@
 import authentication from '@feathersjs/authentication'
+import { HooksObject } from '@feathersjs/feathers'
+import { discard } from 'feathers-hooks-common'
 
-export default {
+const hooks: HooksObject = {
+  error: {
+    create: [
+      context => {
+        context.error.className = 'invalid-login'
+      },
+    ],
+  },
   before: {
     create: [authentication.hooks.authenticate(['local', 'jwt'])],
     remove: [authentication.hooks.authenticate('jwt')],
   },
+  after: {
+    create: [
+      context => {
+        context.result.user = context.params.user
+      },
+      discard('password', 'verifyExpires', 'resetExpires', 'verifyChanges'),
+    ],
+  },
 }
+
+export default hooks
